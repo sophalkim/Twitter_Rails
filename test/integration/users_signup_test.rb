fb8a27c1fd540b75c76b06b7d1490ce386ed994a
Sophal Kim <sophal.kim.work@gmail.com>
2015-01-15 22:44:51 +0000

    Finish Chapter 7 exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'ul'
   end
   
   test "valid signup information" do
@@ -22,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
   end
 end
