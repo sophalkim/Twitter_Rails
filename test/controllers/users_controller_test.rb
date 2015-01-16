e69aa73cdecfb7cc0f380d4c866f759296a4e63e
Sophal Kim <sophal.kim.work@gmail.com>
2015-01-16 22:20:56 +0000

    No unauthorized users to edit test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,12 +4,25 @@ class UsersControllerTest < ActionController::TestCase
   
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
+    @user = users(:michael)
   end
   
   test "should get new" do
     get :new
     assert_response :success
     assert_select "title", "Sign up | #{@base_title}"
+  end
+  
+  test "should redirect edit when no logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect update when no logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
 end
