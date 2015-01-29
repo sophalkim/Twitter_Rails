9b2a227c05e6b0a72dd2edd6765481c745328155
Sophal Kim <sophal.kim.work@gmail.com>
2015-01-29 04:08:43 +0000

    Huge password reset test works

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -24,7 +24,7 @@ class PasswordResetsController < ApplicationController
   end
   
   def update
-    if password_blink?
+    if password_blank?
       flash.now[:danger] = "Password can't be blank"
       render 'edit'
     elsif @user.update_attributes(user_params)
